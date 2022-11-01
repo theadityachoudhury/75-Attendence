343971f9dea0fb1d5c86690a6e460814af761c03
ADITYA NARAYANA CHOUDHURY <69430444+adityasubham03@users.noreply.github.com>
2022-11-02 00:20:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,10 @@
 
 <link rel="icon" type="image/png" href="favicon.ico" />
 
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-
 <link rel="stylesheet" type="text/css" href="css/icon-font.min.css">
-
-<link rel="stylesheet" type="text/css" href="css/animate.css">
-
-<link rel="stylesheet" type="text/css" href="css/hamburgers.min.css">
-
-<link rel="stylesheet" type="text/css" href="css/animation.min.css">
-
-<link rel="stylesheet" type="text/css" href="css/select2.min.css">
 
 <link rel="stylesheet" type="text/css" href="css/daterangepicker.css">
 
-<link rel="stylesheet" type="text/css" href="css/util.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 
 <meta name="robots" content="noindex, follow">
@@ -100,17 +87,7 @@ Calculate
 </div>
 <div id="dropDownSelect1"></div>
 
-<script src="js/jquery-3.2.1.min.js"></script>
-
 <script src="js/animation.min.js"></script>
-
-<script src="js/popper.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
-<script src="js/select2.min.js"></script>
-
-<script src="js/moment.min.js"></script>
-<script src="js/daterangepicker.js"></script>
 
 <script src="js/countdowntime.js"></script>
 
