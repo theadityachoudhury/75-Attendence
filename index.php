56d654f95425b914f3107b8bb30abb1cebb1f8db
ADITYA NARAYANA CHOUDHURY <69430444+adityasubham03@users.noreply.github.com>
2022-11-02 00:02:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,39 @@
 <!doctype html>
 <html lang="en">
-   <head>
-      <!-- Required meta tags -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="icon" href="./screenshots/attendance75website.jpg" type = "image/x-icon">
-      
-      <!-- Bootstrap CSS -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-      
-      <title>Attendance Checker</title>
-   </head>
-   <body>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-         <div class="container-fluid">
-            <a class="navbar-brand" href="/index.php">75 Attendence</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="#">About Us</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="https://privacypolicygen.herokuapp.com/" target="_blank">Policygen</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="/contactus.php">Contact Us</a>
-                  </li>
-               </ul>
-            </div>
-         </div>
-      </nav>
-      <div class="container py-5">
-         <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              
-       <?php
+<head>
+<title>Login V14</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/icon-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+<link rel="stylesheet" type="text/css" href="css/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/animation.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/daterangepicker.css">
+
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<meta name="robots" content="noindex, follow">
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100">
+<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+
+<?php
       
        // Checking the the values submitted or not
                
@@ -50,30 +43,79 @@
        $present = $present > 0 ? $present : 0;
        $total = strip_tags(htmlspecialchars($_POST['total']));
        $total = $total > 0 ? $total : 0;
+       $percent = $_POST['percent'];;
 
        //when data is get then include policy.php and showing the text
           include 'printy.php';
        }else{
           //if submit is not isset then asking to submit the form
        ?>
-               <form action="/" method="post" accept-charset="utf-8">
-                  <div class="form-group">
-                     <label for="present" class="h3" >Present</label>
-                     <input type="number" name="present" value="" class="form-control form-control-lg" id="present" placeholder="Present" required/>
-                  </div>
-                  <div class="form-group">
-                     <label for="total" class="h3" >Total</label>
-                     <input type="number" name="total" value="" class="form-control form-control-lg" id="total" placeholder="Total" required/>
-                  </div>
-               <input type="submit" name="submit" value="Calculate" class="btn btn-primary my-3" required/>
-               </form>
-               
-               <?php } ?>
-             
-            </div>
-         </div>
-      </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>         
-   </body>
-</html>
 
+<form action="/" method="post" accept-charset="utf-8" class="login100-form validate-form flex-sb flex-w">
+<span class="login100-form-title p-b-32">
+75 Attendance
+</span>
+<span class="txt1 p-b-11">
+Present
+</span>
+<div class="wrap-input100 validate-input m-b-36" data-validate="present days required">
+<input class="input100" type="number" min="0" name="present" autocomplete="off">
+<span class="focus-input100"></span>
+</div>
+<span class="txt1 p-b-11">
+Total
+</span>
+<div class="wrap-input100 validate-input m-b-12" data-validate="class days required">
+
+<input class="input100" type="number" min="0" name="total" autocomplete="off">
+<span class="focus-input100"></span>
+</div>
+<div class="input-container percentage-select-container">
+<span class="txt1 p-b-11">
+Percentage
+</span>
+<div>
+        <select name="percent" id="percent" class="percent-select">
+          <option type="number" value="60">60%</option>
+          <option type="number" value="65">65%</option>
+          <option type="number" value="70">70%</option>
+          <option type="number" value="75" selected>75%</option>
+          <option type="number" value="80">80%</option>
+          <option type="number" value="85">85%</option>
+          <option type="number" value="90">90%</option>
+        </select>
+        </div>
+      </div>
+
+
+
+<div class="container-login100-form-btn" style= "padding-top:20px; align-items:center;">
+<button name="submit" class="login100-form-btn"  style="text-align:center" type="submit" value="calculate" >
+Calculate
+</button></div>
+</form>
+<?php } ?>
+</div>
+</div>
+</div>
+<div id="dropDownSelect1"></div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+
+<script src="js/animation.min.js"></script>
+
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/select2.min.js"></script>
+
+<script src="js/moment.min.js"></script>
+<script src="js/daterangepicker.js"></script>
+
+<script src="js/countdowntime.js"></script>
+
+<script src="js/main.js"></script>
+
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"7635535d9e747a1c","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.10.3","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
