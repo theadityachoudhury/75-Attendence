adf8164a759f3cc155155eb9d8f5cca791fcde97
ADITYA NARAYANA CHOUDHURY <69430444+adityasubham03@users.noreply.github.com>
2022-11-04 17:19:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ Total
 <span class="txt1 p-b-11">
 Percentage
 </span>
-<div style="padding-top:15px" name="percent">
+<div style="padding-top:15px" name="percent" id="percent">
         <select>
           <option value="60">60%</option>
           <option value="65">65%</option>
